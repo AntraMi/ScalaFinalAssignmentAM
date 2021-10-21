de2793663488ff35082f0b9a8755e34369ddf59a
Lietotajs <antra.mikelsone@gmail.com>
2021-10-21 21:34:19 +0300

    HangMan drawing is added

--- src/main/scala/FinalAssignmentHangman.scala
+++ src/main/scala/FinalAssignmentHangman.scala
@@ -20,6 +20,19 @@ object FinalAssignmentHangman extends App  {
 
   val alphaSet: Set[Char] = alphaSett
 
+  // Draw nice picture
+
+  def pictureFun (tries:Int): Unit = {
+    tries match {
+      case 0 => println("__\n|  |\n|  O\n| /|\\\n| / \\\n|")
+      case 1 => println("__\n|  |\n|  O\n| /|\\\n| /")
+      case 2 => println("__\n|  |\n|  O\n| /|\\\n| ")
+      case 3 => println("__\n|  |\n|  O\n| /| ")
+      case 4 => println("__\n|  |\n|  O\n| / ")
+      case 5 => println("__\n|  |\n|  O\n|  ")
+    }
+  }
+
 
   //val playerName = readLine("Player what is your name?")
   //println(s"The secret word has $lengthOfWord letters")
@@ -45,12 +58,13 @@ object FinalAssignmentHangman extends App  {
     while (tries > 0 && !guessed && !exit) {
       val guess = readLine("Please guess a letter or a word").toUpperCase()
       if (guess.length == 1 && alphaSet.contains(guess.head)) {
-        if (guessedLetters.contains(guess)) println(s"You already guessed this letter, $guess")
+        if (guessedLetters.contains(guess)) println(s"You already guess this letter, $guess")
         else if (!word.contains(guess.head)) {
           println(s"$guess is not in the word.")
           tries -= 1
+          pictureFun(tries)
           guessedLetters += guess
-          println(s"You have only $tries guesses left")
+          println(s"You have left only $tries guesses")
         }
         else {
           println(s"Good job, $guess is in the word")
@@ -65,10 +79,10 @@ object FinalAssignmentHangman extends App  {
           println(s"Correctly guessed letters $guessedCorrect") // letters are in the order of guess order, not like they are in the secret word
           var ff = guessedCorrect.size
           if (ff == word.length) {
-            println(s"You have guessed all the letters in the word!")
+            println(s"You have guessed all the letter in the word!")
             val guess2 = readLine("Please guess the word").toUpperCase()
             if (guess2 == word) {
-              println(s"You have guessed the word $guess. Congratulations!")
+              println(s"You guess the word $guess, Congratulations!")
               guessed = true
             }
             else if (guess2 != word) {
@@ -76,41 +90,57 @@ object FinalAssignmentHangman extends App  {
               tries -= 6
               guessedWords += guess
               println(s"You lost")
+              println("__\n|  |\n|  O\n| /|\\\n| / \\\n|")
             }
           }
         }
       }
       else if (guess.length == word.length ) {
         if (guess == word) {
-          println(s"You have guessed the word $guess. Congratulations!")
+          println(s"You guess the word $guess, congratulations")
           guessed = true
         }
         else if (guess != word ) {
           println(s"$guess is not the secret word")
           tries -= 1
+          pictureFun(tries)
           guessedWords += guess
-          println(s"You have only $tries guesses left")
+          println(s"You have left only $tries guesses")
         }
         else  {
           println("not a valid guess")
           tries -= 1
-          println(s"You have only $tries guesses left")
+
+          println(s"You have left only $tries guesses")
+          pictureFun(tries)
           guessedWords += guess
         }
       }
       else {
         println("not a valid guess")
         tries -= 1
-        println(s"You have only $tries guesses left")
+        println(s"You have left only $tries guesses")
+        pictureFun(tries)
         guessedWords += guess
 
       }
     }
   }
 
-  play(word)
 
-  val afterGame = readLine("Do you want new game?").toUpperCase()
+  def mainGameLoop(): Unit = {
+    var is_game_needed = true
+    while (is_game_needed) {
+      val response = readLine("New game Y/N ?")
+      if (response.toLowerCase.startsWith("y")) {
+        play(word)
+      }
+      else is_game_needed = false
+      println("Bye bye ")//in this case we end the game on anything other text starting with y or Y
+    }
 
+  }
+
+  mainGameLoop()
 
 }
